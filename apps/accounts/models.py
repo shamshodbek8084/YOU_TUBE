ab8aae38cc7647e63d648dd14643933cfad4f5ae
shamshodbek8084 <erkinmulkamonov@gmail.com>
2025-02-26 02:51:08 +0500

    Channelga model yozildi

---
+++ apps/accounts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from apps.base.models import BaseModel
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Channel(BaseModel):
+    name = models.CharField(max_length=255)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='channel')
+    icon = models.ImageField(upload_to='channel/', blank=True, null=True)
+    desc = models.TextField()
+    banner = models.ImageField(upload_to='channel_banner/', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Channel'
+        verbose_name_plural = 'Channels'
+
+    def __str__(self):
+        return self.name
+
